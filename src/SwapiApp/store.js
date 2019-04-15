78ea26be78181dbf19dd9524d0ff699aac0cd92b
paritosh <paritosh.raj@daffodilsw.com>
2019-04-15 15:32:31 +0530

    updates

--- src/SwapiApp/store.js
+++ src/SwapiApp/store.js
@@ -6,8 +6,8 @@ const middleWare = [thunk];
 const store = createStore(
   reducers,
   compose(
-    applyMiddleware(...middleWare),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleWare)
+    //    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 export default store;
